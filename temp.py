2b776d608b3bbce122576753c1ea0b94ca964af4
N0I0C0K <nick131410@aliyun.com>
2021-02-05 22:40:28 +0800

    简单问题😏

--- temp.py
+++ temp.py
@@ -1,10 +1,16 @@
-for i in range(950, 1010):
-    temp = i
-    f = 0
-    id = 0
-    while temp != 0:
-        x = int(temp%10)
-        f += x*pow(2, id)
-        id += 1
-        temp = int(temp/10)
-    print(f)
+class Solution:
+    def reverse(self, x: int) -> int:
+        s = str(x)
+        a = 0
+        if s[0] == '-':
+            s = s[1:]
+            s = s[::-1]
+            a =  int(s)*-1
+        else:
+            s = s[::-1]
+            a = int(s)
+        if a > 2147483647 or a < -2147483648:
+            return 0
+        return a
+a = Solution()
+print(a.reverse(-1))

5e5c7d046d585bc2bdc806c29033b7946fa0cb4f
N0I0C0K <nick131410@aliyun.com>
2021-02-06 23:51:11 +0800

    2-6😁,滑动窗口

---
+++ 1423. 可获得的最大点数.py
@@ -0,0 +1,25 @@
+#https://leetcode-cn.com/problems/maximum-points-you-can-obtain-from-cards/
+#动态规划 滑动窗口
+
+from typing import List
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        max_ = 0
+        all_ = 0
+        len_c = len(cardPoints)
+        for i in range(len_c):
+            max_ = max_ + cardPoints[i] if i < len_c-k else max_
+            all_ += cardPoints[i]
+        all = max_
+        start, end = 0, len_c - k
+        while end < len_c:
+            all = all - cardPoints[start] + cardPoints[end]
+            start+=1
+            end+=1
+            if all < max_:
+                max_ = all
+        return all_-max_
+
+a = Solution()
+print(a.maxScore([1,2,3,4,5,6,1],3))

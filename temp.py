770d21ca37dcc9fa31bc6805b92ef0163135c2d4
N0I0C0K <nick131410@aliyun.com>
2021-02-08 23:31:16 +0800

    2-8

--- temp.py
+++ temp.py
@@ -8,19 +8,17 @@ class Solution:
         if len_n == 1:
             return True
         m_num = 0
+        num_t = None
         for i in range(len_n):
-            if i == 0:
-                if nums[i] > nums[i+1]:
+            if i < len_n - 1:
+                if num_t is not None:
+                    if nums[i] < num_t:
+                        m_num += 1
+                    else:
+                        num_t = None
+                elif nums[i] > nums[i+1]:
+                    num_t = nums[i]
                     m_num += 1
-                    continue
-            elif i == len_n - 1:
-                if nums[i] < nums[i-1]:
-                    m_num += 1
-                    continue
-            else:
-                if nums[i] < nums[i-1] or nums[i] > nums[i+1]:
-                    m_num += 1
-                    continue
             if m_num >= 2:
                 return False
         return True

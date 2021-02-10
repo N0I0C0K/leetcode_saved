8129cb434a63a9bc738f1a9de342b62f5b9c7236
N0I0C0K <nick131410@aliyun.com>
2021-02-10 14:26:26 +0800

    2-10,BFS😶

--- 787.py
+++ 787.py
@@ -1,8 +1,24 @@
 from typing import List
 
-class node:
-    to_ = []
-
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
-        
+        dist = [[float('inf')] * n for _ in range(n)]
+        for i in range(K+1):
+            dist[src][i] = 0
+        for flight in flights:
+            if flight[0] == src:
+                dist[flight[1]][0] = flight[2]
+        for i in range(K+1):
+            for flight in flights:
+                begin = flight[0]
+                end = flight[1]
+                if dist[begin][i-1] != float('inf'):
+                    dist[end][i] = min(dist[begin][i-1]+flight[2], dist[end][i])
+        return -1 if dist[dst][K] == float('inf') else dist[dst][K]
+
+a = Solution()
+a.findCheapestPrice(3,
+[[0,1,100],[1,2,100],[0,2,500]],
+0,
+2,
+1)

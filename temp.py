4a28978cf84e7f5d2273ca709d8390284d21063f
N0I0C0K <nick131410@aliyun.com>
2021-02-14 11:30:37 +0800

    2-14,大年初三😑

--- temp.py
+++ temp.py
@@ -3,22 +3,15 @@
 from typing import List
 
 class Solution:
-    def checkPossibility(self, nums: List[int]) -> bool:
-        len_n = len(nums)
-        if len_n == 1:
-            return True
-        m_num = 0
-        num_t = None
-        for i in range(len_n):
-            if i < len_n - 1:
-                if num_t is not None:
-                    if nums[i] < num_t:
-                        m_num += 1
-                    else:
-                        num_t = None
-                elif nums[i] > nums[i+1]:
-                    num_t = nums[i]
-                    m_num += 1
-            if m_num >= 2:
-                return False
-        return True
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        MAX = len(nums)
+        num = [0]*(MAX+1)
+        for i in nums:
+            num[i] = i
+        re = []
+        for i in range(1,MAX+1):
+            if num[i] != i:
+                re.append(i)
+        return re
+
+

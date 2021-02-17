cefd224e55abda33b769c06eb6fafb82d4b3b122
N0I0C0K <nick131410@aliyun.com>
2021-02-17 15:30:07 +0800

    Simple😐😐😐

--- temp.py
+++ temp.py
@@ -3,15 +3,21 @@
 from typing import List
 
 class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        max_num = 0
-        len_n = len(nums)
-        temp_num = 0
-        for i in range(len_n):
-            if nums[i] == 1:
-                temp_num += 1
-            else:
-                temp_num = 0
-            if temp_num > max_num:
-                max_num = temp_num
-        return max_num
+    def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
+        h = len(nums)
+        w = len(nums[0])
+        if h*w != r*c:
+            return nums
+        re = []
+        n = 0
+        temp = []
+        for item in nums:
+            for i in item:
+                if n < c:
+                    temp.append(i)
+                    n += 1
+                if n == c:
+                    re.append(temp)
+                    temp = []
+                    n = 0
+        return re

e235b4ca4f343242397da7d586839f2a1f0d157b
N0I0C0K <nick131410@aliyun.com>
2021-03-13 13:13:34 +0800

    简单题

--- temp.py
+++ temp.py
@@ -1,47 +1,20 @@
-# github最近推送有问题
+from typing import List
 
-class MyQueue:
-    
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.res = []
+class Solution:
+    def minElements(self, nums: List[int], limit: int, goal: int) -> int:
+        all = 0
+        for i in nums:
+            all+=i
+        if all<=goal:
+            all = goal - all
+            if all % limit == 0:
+                return int(all/limit)
+            else:
+                return int(all/limit)+1
+        else:
+            all = all-goal
+            if all % limit == 0:
+                return int(all/limit)
+            else:
+                return int(all/limit)+1
 
-
-    def push(self, x: int) -> None:
-        """
-        Push element x to the back of queue.
-        """
-        self.res.append(x)
-
-    def pop(self) -> int:
-        """
-        Removes the element from in front of queue and returns that element.
-        """
-        r = self.res.pop(0)
-        return r
-
-    def peek(self) -> int:
-        """
-        Get the front element.
-        """
-        if len(self.res) >= 1:
-            r = self.res[0]
-            return r
-        return None
-
-    def empty(self) -> bool:
-        """
-        Returns whether the queue is empty.
-        """
-        return not len(self.res) > 0
-
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()

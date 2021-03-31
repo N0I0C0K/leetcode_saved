aa2ab4a3f0e256f1771ce2878587e841f2a6ff24
N0I0C0K <nick131410@aliyun.com>
2021-03-31 20:23:36 +0800

    3-31

--- temp.py
+++ temp.py
@@ -1,4 +1,15 @@
+from typing import List
+
 class Solution:
-    def secondHighest(self, s: str) -> int:
-        for i in s:
-            
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = [[]]
+        for i in range(len(nums)):
+            temp = []
+            for j in range(i, len(nums)):
+                temp.append(nums[j])
+                res.append(temp.copy())
+        return res
+
+a = Solution()
+print(a.subsetsWithDup([1,2,2]))

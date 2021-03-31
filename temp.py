96b879d09a2391f9b06509881f3574a2d3564416
N0I0C0K <nick131410@aliyun.com>
2021-03-31 21:08:54 +0800

    temp

--- temp.py
+++ temp.py
@@ -1,15 +1,16 @@
-from typing import List
+def prime(tar):
+    res = []
+    i = 2
+    while i*i <= tar:
+        if tar%i == 0:
+            while tar%i == 0:
+                res.append(i)
+                tar = int(tar/i)
+        i += 1
+    
+    res.append(tar)
+    return res
 
-class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        res = [[]]
-        for i in range(len(nums)):
-            temp = []
-            for j in range(i, len(nums)):
-                temp.append(nums[j])
-                res.append(temp.copy())
-        return res
-
-a = Solution()
-print(a.subsetsWithDup([1,2,2]))
+while True:
+    a = int(input())
+    print(prime(a))

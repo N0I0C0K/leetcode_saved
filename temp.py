b264dc4f6a356fbee4f06cd424db804175bc7e10
N0I0C0K <nick131410@aliyun.com>
2021-04-16 22:34:47 +0800

    牛客签到题

--- temp.py
+++ temp.py
@@ -1,1 +1,12 @@
-n = input().split()
+a = int(input())
+n = map(int,input().split())
+keys = dict()
+max_n = 0
+for i in n:
+    if i in keys:
+        keys[i] = keys[i]+i
+    else:
+        keys[i] = i
+    if keys[i] > max_n:
+        max_n = keys[i]
+print(max_n)

1f2b87e0d23f9a4b1abddef9296134160ddccb63
N0I0C0K <nick131410@aliyun.com>
2021-05-07 17:13:01 +0800

    easy

--- temp.py
+++ temp.py
@@ -1,3 +1,16 @@
-n = int(input())
-res = (1+(n-1)*6)%1000000007
-print(res)
+from typing import List
+
+class NumArray:
+    sum:list = None
+    def __init__(self, nums: List[int]):
+        temp = 0
+        self.sum = []
+        for num in nums:
+            self.sum.append(temp)
+            temp+=num
+        self.sum.append(temp)
+        pass
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.sum[right+1]-self.sum[left]
+        pass

9043e09c7dd5dee9461af6f87dacab5a291d2d9a
N0I0C0K <nick131410@aliyun.com>
2021-05-07 15:58:18 +0800

    每日dp

---
+++ 64最小路径.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i == 0 and j == 0:
+                    continue
+                if i == 0:
+                    grid[i][j] += grid[i][j-1]
+                elif j == 0:
+                    grid[i][j] += grid[i-1][j]
+                else:
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])#+grid[i][j]
+        return grid[len(grid)-1][len(grid[0])-1]

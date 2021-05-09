b750f398509021c886944b9f88a45aac5c0111c1
N0I0C0K <nick131410@aliyun.com>
2021-05-09 21:43:16 +0800

    重构文件结构

---
+++ leetcode/343.py
@@ -0,0 +1,6 @@
+# 343. 整数拆分 标签:数学，dp
+from typing import List
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        
+        pass
---
+++ leetcode/temp.py
@@ -0,0 +1,2 @@
+n = int(input())
+nums = list(map(int, input().split()))

841aafb3d45f90c0539aa9eee3f6445be4da2663
N0I0C0K <nick131410@aliyun.com>
2021-05-16 09:38:23 +0800

    every

--- leetcode/91. 解码方法.py
+++ leetcode/91. 解码方法.py
@@ -1,21 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         dp = [0]*len(s)
-        dp[0] = 1 if s[0] != '0' else 0
-        for i in range(1, len(s)):
-            if s[i] != '0':
-                dp[i]+=1
-            t:str = s[i-1]+s[i]
-            n = int(t)
-            if s[i-1] != '0':
-                if n > 0 and n < 27:
-                    dp[i]+=1
-            else:
-                if int(s[i-2:i])<27 and int(s[i-2:i]) > 0:
-                    dp[i]+=1
-                if s[i] != '0':
-                    dp[i]+=1
+        dp[0] = 1 if int(s[0]) > 0 else 0
+        for i in range(1,len(s)):
+            if int(s[i]) > 0:
+                dp[i] += dp[i-1]
+            if s[i-1] != '0' and int(s[i-1]+s[i]) > 0 and int(s[i-1]+s[i]) < 27:
+                if i>1: 
+                    dp[i] += dp[i-2]
+                elif i == 1:
+                    dp[i] += 1
         return dp[len(s)-1]
 
 a = Solution()
-print(a.numDecodings('06'))
+print(a.numDecodings('27'))

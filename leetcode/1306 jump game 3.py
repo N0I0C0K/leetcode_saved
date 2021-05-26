98a8997ed51502419b51000c33ef4fa893ef1d00
N0I0C0K <nick131410@aliyun.com>
2021-05-26 09:04:54 +0800

    jump jump

---
+++ leetcode/1306 jump game 3.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        rod = []
+        rod.append(start)
+        len_a = len(arr)
+        is_vis = [False]*len_a
+        while len(rod) > 0:
+            idx = rod.pop(0)
+            is_vis[idx] = True
+            if arr[idx] == 0:
+                return True
+            if idx+arr[idx] < len_a and not is_vis[idx+arr[idx]]:
+                rod.append(idx+arr[idx])
+            if idx-arr[idx] >= 0 and not is_vis[idx-arr[idx]]:
+                rod.append(idx-arr[idx])
+        return False
+
+a = Solution()
+print(a.canReach([4,2,3,0,3,1,2], 0))

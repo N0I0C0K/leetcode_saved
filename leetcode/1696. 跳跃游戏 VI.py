91277539774785be30611a5b817a58227ee3a468
N0I0C0K <nick131410@aliyun.com>
2021-05-28 11:58:57 +0800

    5-28,dp+最大堆

---
+++ leetcode/1696. 跳跃游戏 VI.py
@@ -0,0 +1,26 @@
+from typing import List
+import heapq
+
+'''
+heapq 默认最小堆,通过插入变负数改变单调性来变成最大堆.
+
+'''
+
+
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        len_n = len(nums)
+        dp = [-1e9]*(len_n)
+        heap = []
+        dp[0] = nums[0]
+        heapq.heappush(heap, (-dp[0], 0))
+        for i in range(1,len_n):
+            while i - heap[0][1] > k:
+                heapq.heappop(heap)
+            dp[i] = -heap[0][0]+nums[i]
+            #heapq.heappop(heap)
+            heapq.heappush(heap, (-dp[i], i))
+        return dp[len_n-1]
+
+a = Solution()
+print(a.maxResult([1,-5,-20,4,-1,3,-6,-3],2))

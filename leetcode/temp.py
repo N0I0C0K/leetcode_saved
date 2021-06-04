9908f099ba0af9cd3af7894cbc085dc383faf314
N0I0C0K <nick131410@aliyun.com>
2021-06-04 17:57:51 +0800

    update

--- leetcode/temp.py
+++ leetcode/temp.py
@@ -1,18 +1,11 @@
-
-import turtle
-import time
- 
-turtle.pensize(5)
-turtle.pencolor("yellow")
-turtle.fillcolor("red")
- 
-turtle.begin_fill()
-for _ in range(5):
-  turtle.forward(200)
-  turtle.right(144)
-turtle.end_fill()
-time.sleep(2)
- 
-turtle.penup()
- 
-turtle.mainloop()
+n,t = map(int, input().split())
+cost = list(map(int, input().split()))
+cost.sort()
+res = 0
+for i in range(n):
+    if res+cost[i] > t:
+        print(i)
+        break
+    else:
+        res+=cost[i]
+        pass

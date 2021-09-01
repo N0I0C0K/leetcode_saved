8469fab56a48695481899d38c245a015fa98d9f1
N0I0C0K <nick131410@aliyun.com>
2021-09-01 23:29:00 +0800

    !超级大坑 1.但凡涉及到字符串一定要采用%s输入 2.map之类的不要clear()直接new一个 3.一定要多想几组数据

--- ccpc/b.cpp
+++ ccpc/b.cpp
@@ -1,40 +1,43 @@
 #include<cstdio>
 #include<map>
+#include<unordered_map>
+#include<string>
 using namespace std;
 using ll = long long;
 using uint = unsigned int;
 const int inf = 0x3f3f3f3f;
 
 char ss[100010];
+
 int main()
 {
     int t,l;
     char temp;
-    map<ll,bool> keys;
     scanf("%d",&t);
     while (t--)
     {
-        keys.clear();
-        ll x = 0,y = 0,res = 0;
-        keys[0] = true;
+        unordered_map<ll,ll> keys;
+        ll h = 0;
+        ll res = 0;
+        keys[0] = 1;
         scanf("%d",&l);
         scanf("%s",ss);
         for (int i = 0; i < l; ++i)
         {
             temp = ss[i];
             if(temp == 'U')
-                ++y;
+                h+=100000;
             else if(temp == 'D')
-                --y;
+                h-=100000;
             else if(temp == 'L')
-                --x;
+                --h;
             else if(temp == 'R')
-                ++x;
-            ll h = x+y*1000000;
-            if(keys.find(h) != keys.end())
-                res++;
-            else
-                keys[h] = true;
+                ++h;
+            if(keys.find(h)!=keys.end())
+            {
+                res+=keys[h];
+            }
+            keys[h]++;
         }
         printf("%lld\n",res);
     }

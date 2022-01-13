02c22be96095e41cb6ca7cf4d3ff35087952ee37
N0I0C0K <nick131410@aliyun.com>
2022-01-13 09:32:53 +0800

    每日一题

--- leetcode/temp.py
+++ leetcode/temp.py
@@ -1,28 +1,17 @@
-def update(tar: list) -> list:
-    res = []
-    for i in range(len(tar)):
-        isT = False
-        for j in range(len(tar)):
-            if i != j and len(tar[j]) > len(tar[i]) and tar[i] == tar[j][len(tar[j])-len(tar[i]):]:
-                isT = True
-                break
-        if not isT and tar[i] not in res:
-            res.append(tar[i])
-    return res
+from typing import *
 
 
-user = dict()
-nums = int(input())
-for i in range(nums):
-    info = input().split()
-    if info[0] in user:
-        user[info[0]] += info[2:]
-    else:
-        user[info[0]] = info[2:]
-    user[info[0]] = update(user[info[0]])
-print(len(user.keys()))
-for key, item in user.items():
-    print(f'{key} {len(item)}', end='')
-    for i in item:
-        print(f' {i}', end='')
-    print('')
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+        nums = list(enumerate(nums))
+        nums.sort(reverse=True, key=lambda x: x[1])
+        if nums[0][1] >= nums[1][1]*2:
+            return nums[0][0]
+        else:
+            return -1
+
+
+a = Solution()
+print(a.dominantIndex([3, 6, 1, 0]))

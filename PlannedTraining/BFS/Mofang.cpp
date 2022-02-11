e8f9259937f7b9000d5a899b3cff936dbf33c217
N0I0C0K <nick131410@aliyun.com>
2022-02-11 21:08:59 +0800

    每日一题

---
+++ PlannedTraining/BFS/Mofang.cpp
@@ -0,0 +1,15 @@
+#include <queue>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+const int inf = 0x3f3f3f3f;
+
+int map[4][6];
+
+int main()
+{
+    return 0;
+}
--- leetcode/temp.py
+++ leetcode/temp.py
@@ -2,14 +2,13 @@ from typing import *
 
 
 class Solution:
-    def countKDifference(self, nums: List[int], k: int) -> int:
-        ans = 0
-        cnt = Counter()
-        for a in nums:
-            ans += cnt[a-k]+cnt[a+k]
-            cnt[a] += 1
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans = 1000010
+        for i in range(k-1, len(nums)):
+            ans = min(ans, nums[i]-nums[i-k+1])
         return ans
 
 
 a = Solution()
-print(a.countKDifference([1, 3], 2))
+print(a.minimumDifference([9, 4, 1, 7], 2))

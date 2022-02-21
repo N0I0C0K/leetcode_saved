6fce7a9ff6db8245a72a04ead878658b7a6c4f8a
N0I0C0K <nick131410@aliyun.com>
2022-02-21 11:06:59 +0800

    每日一题

--- leetcode/temp.py
+++ leetcode/temp.py
@@ -2,17 +2,14 @@ from typing import *
 
 
 class Solution:
-    def maxNumberOfBalloons(self, text: str) -> int:
-        ans = {key: 0 for key in 'balloon'}
-        for i in text:
-            if i in ans:
-                if i in 'lo':
-                    ans[i] += 1
-                else:
-                    ans[i] += 2
-        r = min(ans.values())
-        return r >> 1 if (r & 1 == 0) else (r-1) >> 1
-
-
-a = Solution()
-print(a.maxNumberOfBalloons('ballon'))
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        l = len(bits)
+        i = 0
+        while i < l:
+            if i + 1 == l:
+                return True
+            a = bits[i]
+            if a == 1:
+                i += 1
+            i += 1
+        return False

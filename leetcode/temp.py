475a6e41d9c3eaeff3159beaa5aa8d5f5a082dee
N0I0C0K <nick131410@aliyun.com>
2022-03-18 21:43:04 +0800

    每日一题

--- leetcode/temp.py
+++ leetcode/temp.py
@@ -1,10 +1,27 @@
 from typing import *
 
 
-class Solution:
-    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
-        res = 1e5
-        for i, t in enumerate(nums):
-            if t == target:
-                res = min(abs(i-start), res)
-        return res
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+        self.n = len(balance)
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if 1 <= account1 <= self.n and 1 <= account2 <= self.n and self.balance[account1-1] >= money:
+            self.balance[account1-1] -= money
+            self.balance[account2-1] += money
+            return True
+        return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        if 1 <= account <= self.n:
+            self.balance[account-1] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if 1 <= account <= self.n and self.balance[account-1] >= money:
+            self.balance[account-1] -= money
+            return True
+        return False

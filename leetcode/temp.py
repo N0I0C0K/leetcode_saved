68fb99ce687ff05e612a349a6ed60521946a2475
N0I0C0K <nick131410@aliyun.com>
2022-04-03 11:02:27 +0800

    每日一题

--- leetcode/temp.py
+++ leetcode/temp.py
@@ -2,21 +2,17 @@ from typing import *
 
 
 class Solution:
-    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        res = []
-
-        def check(n: int) -> bool:
-            n_s = str(n)
-            for i in n_s:
-                if i == '0' or n % int(i) != 0:
-                    return False
-            return True
-
-        for i in range(left, right+1):
-            if check(i):
-                res.append(i)
-        return res
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        lef, rig = 0, len(letters)
+        t = ord(target)
+        while lef < rig:
+            mid = (lef+rig) >> 1
+            if ord(letters[mid]) > t:
+                rig = mid
+            else:
+                lef = mid+1
+        return letters[rig] if rig < len(letters) else letters[0]
 
 
 a = Solution()
-print(a.selfDividingNumbers(1, 22))
+print(a.nextGreatestLetter(["c", "f", "j"], 'c'))

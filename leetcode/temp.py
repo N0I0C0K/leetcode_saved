342596575da659c37c3d0e514b30501cadd83d8b
N0I0C0K <nick131410@aliyun.com>
2022-04-05 13:59:26 +0800

    每日一题

--- leetcode/temp.py
+++ leetcode/temp.py
@@ -2,17 +2,26 @@ from typing import *
 
 
 class Solution:
-    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        lef, rig = 0, len(letters)
-        t = ord(target)
-        while lef < rig:
-            mid = (lef+rig) >> 1
-            if ord(letters[mid]) > t:
-                rig = mid
-            else:
-                lef = mid+1
-        return letters[rig] if rig < len(letters) else letters[0]
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        def check(x: int) -> bool:
+            nums1 = 0
+            while x > 0:
+                nums1 += x % 2
+                x //= 2
+            if nums1 == 1:
+                return False
+            for i in range(2, nums1):
+                if i*i > nums1:
+                    break
+                if nums1 % i == 0:
+                    return False
+            return True
+        ans = 0
+        for i in range(left, right+1):
+            if check(i):
+                ans += 1
+        return ans
 
 
 a = Solution()
-print(a.nextGreatestLetter(["c", "f", "j"], 'c'))
+print(a.countPrimeSetBits(10, 15))
